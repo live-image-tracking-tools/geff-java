78acbddb7485c01127ee26a91c356c6c4acc83e3
Ko Sugawara <ko.sugawara@riken.jp>
2025-07-22 09:51:52 +0900

    Add round-trip test for creating geff zarr

---
+++ src/test/java/org/mastodon/geff/GeffCreateTest.java
@@ -0,0 +1,84 @@
+package org.mastodon.geff;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ucar.ma2.InvalidRangeException;
+
+public class GeffCreateTest
+{
+
+        public static void main( String[] args ) throws IOException, InvalidRangeException
+        {
+                List< GeffNode > newNodes = new ArrayList<>();
+                GeffNode node0 = new GeffNode.Builder()
+                                .id( 0 )
+                                .timepoint( 0 )
+                                .x( 10.5 )
+                                .y( 20.3 )
+                                .z( 5.0 )
+                                .segmentId( 0 )
+                                .color( new double[] { 1.0, 0.0, 0.0, 1.0 } ) // Red
+                                                                              // color
+                                .radius( 2.5 )
+                                // .covariance2d(new double[] { 1.0, 0.2, 0.2,
+                                // 1.5 }) // 2x2 covariance matrix
+                                // flattened
+                                .build();
+                newNodes.add( node0 );
+
+                GeffNode node1 = new GeffNode.Builder()
+                                .id( 1 )
+                                .timepoint( 1 )
+                                .x( 11.5 )
+                                .y( 21.3 )
+                                .z( 6.0 )
+                                .segmentId( 1 )
+                                // .covariance2d(new double[] { 0.8, 0.1, 0.1,
+                                // 1.2 }) // Different covariance
+                                .build();
+                newNodes.add( node1 );
+
+                // Write to Zarr format with version specification
+                GeffNode.writeToZarr( newNodes,
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks/nodes",
+                                "0.3.0" );
+
+                // Create new edges using builder pattern
+                List< GeffEdge > newEdges = new ArrayList<>();
+                GeffEdge edge = new GeffEdge.Builder()
+                                .setId( 0 )
+                                .setSourceNodeId( 0 )
+                                .setTargetNodeId( 1 )
+                                .setScore( 0.95 )
+                                .setDistance( 1.4 )
+                                .build();
+                newEdges.add( edge );
+
+                // Write to Zarr format
+                GeffEdge.writeToZarr( newEdges,
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks/edges",
+                                "0.3.0" );
+
+                // Create metadata with axis information
+                GeffAxis[] axes = {
+                                new GeffAxis( GeffAxis.NAME_TIME, GeffAxis.TYPE_TIME, GeffAxis.UNIT_SECONDS, 0.0, 100.0 ),
+                                new GeffAxis( GeffAxis.NAME_SPACE_X, GeffAxis.TYPE_SPACE, GeffAxis.UNIT_MICROMETERS, 0.0, 1024.0 ),
+                                new GeffAxis( GeffAxis.NAME_SPACE_Y, GeffAxis.TYPE_SPACE, GeffAxis.UNIT_MICROMETERS, 0.0, 1024.0 ),
+                                new GeffAxis( GeffAxis.NAME_SPACE_Z, GeffAxis.TYPE_SPACE, GeffAxis.UNIT_MICROMETERS, 0.0, 100.0 )
+                };
+                GeffMetadata metadata = new GeffMetadata( "0.3.0", true, axes );
+                GeffMetadata.writeToZarr( metadata,
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks" );
+
+                GeffMetadata metadata_read = GeffMetadata.readFromZarr(
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks" );
+                List< GeffNode > nodes = GeffNode.readFromZarr(
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks",
+                                metadata_read.getGeffVersion() );
+                List< GeffEdge > edges = GeffEdge.readFromZarr(
+                                "/Users/sugawara/Repositories/geff-java/src/test/resources/create_test_output.zarr/tracks",
+                                metadata_read.getGeffVersion() );
+        }
+}
