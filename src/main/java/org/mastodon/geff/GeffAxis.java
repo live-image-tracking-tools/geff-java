0b097a54343a122cf7bf5a8371e8c287eeba6c0f
Ko Sugawara <ko.sugawara@riken.jp>
2026-06-24 01:25:20 +0900

    Update license statements

--- src/main/java/org/mastodon/geff/GeffAxis.java
+++ src/main/java/org/mastodon/geff/GeffAxis.java
@@ -2,7 +2,7 @@
  * #%L
  * geff-java
  * %%
- * Copyright (C) 2025 Ko Sugawara
+ * Copyright (C) 2025 - 2026 Ko Sugawara
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
