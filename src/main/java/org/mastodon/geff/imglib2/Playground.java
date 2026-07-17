d5d13df5c9a5c379871ba98d3476bff2ae12d772
tpietzsch <tobias.pietzsch@gmail.com>
2026-07-17 21:59:59 +0200

    WIP

--- src/main/java/org/mastodon/geff/imglib2/Playground.java
+++ src/main/java/org/mastodon/geff/imglib2/Playground.java
@@ -1,18 +1,16 @@
 package org.mastodon.geff.imglib2;
 
-import net.imglib2.Positionable;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.Sampler;
 import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.type.numeric.IntegerType;
-import net.imglib2.type.numeric.NumericType;
-import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.integer.UnsignedLongType;
+import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.util.Cast;
 import net.imglib2.util.Intervals;
-import org.janelia.saalfeldlab.n5.RawCompression;
+import net.imglib2.util.Util;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 import org.janelia.saalfeldlab.n5.zarr.DType;
 import org.janelia.saalfeldlab.n5.zarr.N5ZarrReader;
@@ -32,7 +30,7 @@ public class Playground {
     // -----------------------------------------------------------------------
     // client-side
     //
-    record Vertex(long id) {
+    record Vertex(long id, double x) {
     }
 
 
@@ -46,7 +44,7 @@ public class Playground {
         private final List<SlicePosition> slicePositions = new ArrayList<>();
         private final Slice<UnsignedLongType> nodeIdSlice;
 
-        private final RandomAccess<UnsignedLongType> id;
+        private final HashMap<String, RandomAccessibleInterval<?>> nodeProperties = new HashMap<>();
         private final HashMap<String, RandomAccess<?>> nodeProperyAccesses = new HashMap<>();
 
         // TODO:                          generic int type here?
@@ -55,10 +53,9 @@ public class Playground {
             numNodes = nodeIdData.dimension(0);
             nodeIdSlice = Slice.slice(nodeIdData, 0);
 
+            nodeProperties.put("id", nodeIdSlice);
             slicePositions.add(nodeIdSlice.slicePosition());
-
-            id = nodeIdSlice.randomAccess();
-            nodeProperyAccesses.put("id", id);
+            nodeProperyAccesses.put("id", nodeIdSlice.randomAccess());
         }
 
         long size() {
@@ -75,6 +72,17 @@ public class Playground {
             return nodeProperyAccesses.get(property);
         }
 
+        public void addProperty(final String identifier, final RandomAccessibleInterval<?> data) {
+
+            final int n = data.numDimensions();
+            if (data.dimension(n-1) != numNodes)
+                throw new IllegalArgumentException("property \"" + identifier + "\": dimensions don't match number of nodes (" + data.dimension(n - 1) + "!=" + numNodes + ")");
+
+            Slice<?> propSlice = Slice.slice(data, n - 1);
+            nodeProperties.put(identifier, propSlice);
+            slicePositions.add(propSlice.slicePosition());
+            nodeProperyAccesses.put(identifier, propSlice.randomAccess());
+        }
     }
 
 
@@ -82,24 +90,30 @@ public class Playground {
     // -----------------------------------------------------------------------
 
     public static void main(String[] args) {
-        final String path = "cross-language-tests/data/basic_3d_original.zarr";
+//        final String path = "cross-language-tests/data/basic_3d_original.zarr";
+        final String path = "cross-language-tests/data/covariance_original.zarr";
 
         final List<Vertex> nodes = new ArrayList<>();
 
         // read
         try (final N5ZarrReader n5 = new N5ZarrReader(path)) {
 
-            final String dataset = "nodes/ids";
-            final ZarrDatasetAttributes attributes = (ZarrDatasetAttributes) n5.getDatasetAttributes(dataset);
-            System.out.println("attributes.getDType() = " + attributes.getDType());
+            printAttributes(n5, "nodes/ids");
+            printAttributes(n5, "nodes/props/x/values");
+//            printAttributes(n5, "nodes/props/covariance2d/values");
+//            printAttributes(n5, "nodes/props/covariance3d/values");
 
-            final RandomAccessibleInterval<UnsignedLongType> ids = N5Utils.open(n5, dataset);
+            final RandomAccessibleInterval<UnsignedLongType> ids = N5Utils.open(n5, "nodes/ids");
+            final RandomAccessibleInterval<DoubleType> xs = N5Utils.open(n5, "nodes/props/x/values");
+
             final NodeData nodeData = new NodeData(ids);
+            nodeData.addProperty("x", xs);
 
             final Sampler<UnsignedLongType> id = Cast.unchecked(nodeData.getPropertyAccess("id"));
+            final Sampler<DoubleType> x = Cast.unchecked(nodeData.getPropertyAccess("x"));
             for (int i = 0; i < nodeData.size(); i++) {
                 nodeData.nodeIndex(i);
-                nodes.add(new Vertex(id.get().get()));
+                nodes.add(new Vertex(id.get().get(), x.get().get()));
             }
 
             print(nodes);
@@ -108,32 +122,52 @@ public class Playground {
         // write
         try (final N5ZarrWriter n5 = new N5ZarrWriter(path)) {
 
-            final String dataset = "nodes/ids2";
-            final ZarrDatasetAttributes attributes = new ZarrDatasetAttributes(
-                    new long[]{nodes.size()},
-                    new int[]{nodes.size()},
-                    new DType("<u8", null),
-                    new ZstandardCompression(0),
-                    true,
-                    "0"
-            );
-
             final RandomAccessibleInterval<UnsignedLongType> ids = ArrayImgs.unsignedLongs(nodes.size());
+            final RandomAccessibleInterval<DoubleType> xs = ArrayImgs.doubles(nodes.size());
+
             final NodeData nodeData = new NodeData(ids);
+            nodeData.addProperty("x", xs);
 
             final Sampler<UnsignedLongType> id = Cast.unchecked(nodeData.getPropertyAccess("id"));
+            final Sampler<DoubleType> x = Cast.unchecked(nodeData.getPropertyAccess("x"));
             for (int i = 0; i < nodes.size(); i++) {
                 nodeData.nodeIndex(i);
                 id.get().set(nodes.get(i).id());
+                x.get().set(nodes.get(i).x());
             }
 
-            n5.createDataset(dataset, attributes);
-            N5Utils.saveRegion(ids,n5,dataset, attributes);
+            writeDataset(n5, "nodes/ids2", "<u8", ids);
+            writeDataset(n5, "nodes/props/x/values2", "<f8", xs);
         }
-
     }
 
-    private static void print(List<Vertex> nodes) {
+    private static <T extends NativeType<T>> void writeDataset(
+            final N5ZarrWriter n5,
+            final String dataset,
+            final String typestr,
+            final RandomAccessibleInterval<T> data) {
+        final long[] dimensions = data.dimensionsAsLongArray();
+        final int[] blockSize = Util.long2int(dimensions);
+        final ZarrDatasetAttributes attributes = new ZarrDatasetAttributes(
+                dimensions,
+                blockSize,
+                new DType(typestr, null),
+                new ZstandardCompression(0),
+                true,
+                "0"
+        );
+        n5.createDataset(dataset, attributes);
+        N5Utils.saveRegion(data, n5, dataset, attributes);
+    }
+
+    private static void printAttributes(final N5ZarrReader n5, final String dataset) {
+        final ZarrDatasetAttributes attributes = (ZarrDatasetAttributes) n5.getDatasetAttributes(dataset);
+        System.out.println(dataset + ":");
+        System.out.println("  DType=\"" + attributes.getDType() + "\"");
+        System.out.println("  dimensions=\"" + Arrays.toString(attributes.getDimensions()) + "\"");
+    }
+
+    private static void print(final List<Vertex> nodes) {
         nodes.forEach(System.out::println);
     }
 
