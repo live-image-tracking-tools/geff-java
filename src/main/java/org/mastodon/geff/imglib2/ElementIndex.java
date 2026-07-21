00c13ca20c67df8e7ab6264e5285e2745f39225f
tpietzsch <tobias.pietzsch@gmail.com>
2026-07-22 00:37:50 +0200

    TODO

--- src/main/java/org/mastodon/geff/imglib2/ElementIndex.java
+++ src/main/java/org/mastodon/geff/imglib2/ElementIndex.java
@@ -4,10 +4,12 @@ public final class ElementIndex {
 
     final long[] slicePos = new long[1];
 
+    // TODO rename to get()
     public long index() {
         return slicePos[0];
     }
 
+    // TODO rename to set()
     public void index( final long index ) {
         slicePos[0] = index;
     }
